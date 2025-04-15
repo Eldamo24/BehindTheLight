50b5390e4844966e20343ec1730a3c85c1d95922
Damian Lobos <damianlobos24@gmail.com>
2025-04-15 10:20:32 -0300

    Lantern working

--- BehindTheLight/Assets/Scripts/Light scripts/LightedObjects.cs
+++ BehindTheLight/Assets/Scripts/Light scripts/LightedObjects.cs
@@ -1,14 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class LightedObjects : MonoBehaviour, IInteractable
 {
     [SerializeField] private Material mat;
+    [SerializeField] private TMP_Text textObj;
     private float fadeSpeed = 1.5f;
     private Coroutine fadeCoroutine;
     [SerializeField] private string onInteractMsg;
 
     public string OnInteractMsg => onInteractMsg;
+
+    private void Start()
+    {
+        TryGetComponent<TMP_Text>(out textObj);
+        if(textObj != null)
+        {
+            Color c = textObj.color;
+            c.a = 0f;
+            textObj.color = c;
+        }
+    }
 
     public void StartFadeIn()
     {
@@ -33,22 +46,45 @@ public class LightedObjects : MonoBehaviour, IInteractable
     /// </summary>
     IEnumerator FadeTo(float alpha)
     {
-        Color c = mat.color;
+        Color c;
+        if(textObj != null)
+        {
+            c = textObj.color;
+        }
+        else
+        {
+            c = mat.color;
+        }
         float startAlpha = c.a;
 
         while (Mathf.Abs(c.a - alpha) > 0.01f)
         {
             c.a = Mathf.MoveTowards(c.a, alpha, fadeSpeed * Time.deltaTime);
-            mat.color = c;
+            if(textObj != null)
+            {
+                textObj.color = c;
+            }
+            else
+            {
+                mat.color = c;
+            }
             yield return null;
         }
-
         c.a = alpha;
-        mat.color = c;
+        if (textObj != null)
+        {
+            textObj.color = c;
+        }
+        else
+        {
+            mat.color = c;
+        }
     }
 
     public bool IsVisible()
     {
+        if (textObj != null)
+            return textObj.color.a > 0.95f;
         return mat.color.a > 0.95f;
     }
 
@@ -59,9 +95,23 @@ public class LightedObjects : MonoBehaviour, IInteractable
 
     private void OnDestroy()
     {
-        Color c = mat.color;
+        Color c;
+        if (textObj != null)
+        {
+            c = textObj.color;
+        }
+        else
+        {
+            c = mat.color;
+        }
         c.a = 0f;
-        mat.color = c;
+        if (textObj != null)
+        {
+            textObj.color = c;
+        }
+        else
+        {
+            mat.color = c;
+        }
     }
 }
-
