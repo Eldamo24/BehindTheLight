fd6cb7a3050a29dee2e66ebfaca82f99c47f2fc8
Damian Lobos <damianlobos24@gmail.com>
2025-04-21 18:33:20 -0300

    State machine and states

---
+++ BehindTheLight/Assets/Scripts/Enemy/Enemy States/AttackState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/States/Attack")]
+public class AttackState : EnemyState
+{
+    public override void Enter(EnemyStateMachine stateMachine)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState(EnemyStateMachine stateMachine)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit(EnemyStateMachine stateMachine)
+    {
+        throw new System.NotImplementedException();
+    }
+}
